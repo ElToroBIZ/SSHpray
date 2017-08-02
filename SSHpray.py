abad3a6f8ad06e89fbf5b2ca6cce23a2747be1e4
bharshbarger <bharshbarger@users.noreply.github.com>
2017-08-02 18:05:23 -0500

    Update SSHpray.py

--- SSHpray.py
+++ SSHpray.py
@@ -170,16 +170,17 @@ class SSHpray():
                     #server response not working for some reason
                     print ('[+] {} responded to {} with: \n'.format(t,c))
 
+                    output = ''.join(stdout.readlines())
+                    print (output)
+
                     #create dir if missing
                     if not os.path.exists(self.loot_dir+str(t)):
                         os.makedirs(self.loot_dir+str(t))
 
                     #save output to a file 
                     c = c.translate(None, '~!@#$%^&*()_+`-=[]\|/?.,:;<>')
-                    with open(self.loot_dir+str(c)+'_loot.txt', 'w') as loot_file:
-                        loot_file.writelines(''.join(stdout.readlines()))
-
-                    print (''.join(stdout.readlines()))
+                    with open(self.loot_dir+t+'/'+str(c)+'_loot.txt', 'w') as loot_file:
+                        loot_file.writelines(output)
 
 
                     print('\n')
